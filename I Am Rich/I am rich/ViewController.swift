5badab15dedf0b4636cb556a6b48c9a6302891f1
vitória <142357551+similvitoria@users.noreply.github.com>
2024-07-03 01:16:20 -0300

    Adicionei o app 1, I Am Rich

---
+++ I Am Rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Vitória Símil on 02/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
